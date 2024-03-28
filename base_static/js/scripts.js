fbcb90ca9fb95d78b8aa28e1967ccaa55b078e9c
Daniel Antunes <antunesdaniel083@gmail.com>
2024-03-28 11:19:35 -0300

    criando a funcionalidade do js monitorar o input de valor para aceitar apenas valores numéricos

--- base_static/js/scripts.js
+++ base_static/js/scripts.js
@@ -1,11 +1,38 @@
-var currentUrl = window.location.pathname;
-var navlinks = document.querySelectorAll("nav a");
+const currentUrl = window.location.pathname;
+const navlinks = document.querySelectorAll("nav a");
+const value_input = document.querySelector(".value_rs");
+
+const formatRealInput = (text) => {
+    clearValue_ = text.replace(/[^0-9,]/g, "");
+
+    if (clearValue_.split(",").length > 2) {
+        clearValue_ = clearValue_.slice(0, -1);
+        return clearValue_;
+    }
+
+    const parts = clearValue_.split(",");
+
+    if (parts.length == 2 && parts[1].length > 2) {
+        clearValue_ = parts[0] + "," + parts[1].slice(0, 2);
+        return clearValue_;
+    }
+
+    return clearValue_;
+};
 
 function activeLink(currentUrl, navlinks) {
     navlinks.forEach((link) => {
         if (link.getAttribute("href") === currentUrl) {
             link.classList.add("active");
         }
+    });
+}
+
+if (currentUrl === "/") {
+    value_input.addEventListener("input", (e) => {
+        updated_value = formatRealInput(e.target.value);
+        e.target.value = updated_value;
+        e.stopPropagation();
     });
 }
 
